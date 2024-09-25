3f0162d9ae455c2d75a3a4ff9adbc95dcc109ebc
Pedro Daguer <pepedavila1308@gmail.com>
2024-09-25 11:39:43 +0100

    Código finalizado, realizando o desafio proposto.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -4,16 +4,20 @@ public class ContaTerminal {
 
     public static void main(String[] args) {
 
-        int NumeroConta;
-        String Agencia;
-        String NomeClinte;
-        float Saldo;
-
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Seja bem-vindo ao terminal! \nPor Favor insira seu número de conta: ");
-        NumeroConta = input.nextInt();
-        System.out.println(NumeroConta);
+        System.out.println("\nSeja bem-vindo ao terminal! \n\nPor Favor insira seu número de conta: ");
+        int NumeroConta = Integer.parseInt(input.nextLine());
 
+        System.out.println("Agora insira o número da sua agência: ");
+        String Agencia = input.nextLine();
+
+        System.out.println("Por favor insira seu nome: ");
+        String NomeCliente = input.nextLine();
+
+        System.out.println("Por favor insira seu saldo: ");
+        float Saldo = input.nextFloat();
+
+        System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: " + Agencia + ", conta " + NumeroConta + " e seu saldo de " + Saldo + " já está disponível para saque.");
+        }
     }
-}
