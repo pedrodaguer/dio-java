bcbad2b7f5da3d986cd195efc8c19cea15f7f4a5
Pedro Daguer <pepedavila1308@gmail.com>
2024-09-25 11:23:02 +0100

    Código Inicial

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,9 +1,19 @@
-
+import java.util.Scanner;
 
 public class ContaTerminal {
 
-    int NumeroConta;
+    public static void main(String[] args) {
 
+        int NumeroConta;
+        String Agencia;
+        String NomeClinte;
+        float Saldo;
 
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Seja bem-vindo ao terminal! \nPor Favor insira seu número de conta: ");
+        NumeroConta = input.nextInt();
+        System.out.println(NumeroConta);
+
+    }
 }
